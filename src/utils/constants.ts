bf534f7ae4e84b55edab2330d4f6f4a17332e3e8
AChirkova <leadenrain@yandex.ru>
2024-08-03 10:40:56 +0500

    add const

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,5 +2,8 @@ export const API_URL = `${process.env.API_ORIGIN}/api/weblarek`;
 export const CDN_URL = `${process.env.API_ORIGIN}/content/weblarek`;
 
 export const settings = {
-
+	headers: {
+		authorization: `${process.env.API_TOKEN}`,
+		'Content-Type': 'application/json',
+	},
 };
