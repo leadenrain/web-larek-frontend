2b460ee731973eba79ba90341c30972dec1d3c91
AChirkova <leadenrain@yandex.ru>
2024-08-11 15:32:45 +0500

    add const

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,3 +7,11 @@ export const settings = {
 		'Content-Type': 'application/json',
 	},
 };
+
+export const categories: Record<string, string> = {
+	'софт-скил': 'card__category_soft',
+	'хард-скил': 'card__category_hard',
+	другое: 'card__category_other',
+	дополнительное: 'card__category_additional',
+	кнопка: 'card__category_button',
+};
