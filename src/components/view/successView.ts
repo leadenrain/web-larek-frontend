e901948638fd088765c0c2270d7355991f241f7d
AChirkova <leadenrain@yandex.ru>
2024-08-11 20:44:22 +0500

    add SuccessView

---
+++ src/components/view/successView.ts
@@ -0,0 +1,28 @@
+import { cloneTemplate } from '../../utils/utils';
+import { Component } from '../base/componentView';
+import { IEvents } from '../base/events';
+
+type TSuccessView = {
+	message: string;
+};
+
+export class SuccessView extends Component<TSuccessView> {
+	protected _message: HTMLParagraphElement;
+	protected button: HTMLButtonElement;
+
+	constructor(template: HTMLTemplateElement, protected events: IEvents) {
+		const element = cloneTemplate<HTMLElement>(template);
+		super(element);
+
+		this._message = this.container.querySelector('.order-success__description');
+		this.button = this.container.querySelector('.order-success__close');
+
+		this.button.addEventListener('click', () => {
+			this.events.emit('order: success');
+		});
+	}
+
+	set message(total: number) {
+		this._message.textContent = `Списано ${total} синапсов`;
+	}
+}
